e2d99605c1090768d7e4c606cfb7273ede2dc8fb
Rok Grgič Meško <rok.grgic-mesko@tum.de>
2023-11-16 17:21:18 +0100

    Fix typo

--- src/gui/Game.cpp
+++ src/gui/Game.cpp
@@ -36,7 +36,7 @@ void Game::runGame()
         // Set background grey color.
         m_window.clear(sf::Color::Color(128, 128, 128, 255));
 
-        // TODO: Change this so, sprite is class member.
+        // TODO: Change this so sprite is a class member, then handle resize with sprite resize().
         auto boardSprite = m_board.getBoardSprite();
         boardSprite.setPosition(0.5f * static_cast<sf::Vector2f>(m_windowSize));
 
